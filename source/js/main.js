3064b556e0d317544debe50ac0a94c4dcab638d6
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2023-03-02 22:42:14 +0400

    Добавляет стилизацию подвала

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {accordion} from './modules/accordion/spoller';
 
 // ---------------------------------
 
@@ -13,6 +14,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Modules
   // ---------------------------------
+
+  accordion();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
