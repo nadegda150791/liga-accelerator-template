5049f853dad12ceb5a01dd54c670b4df8857119d
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2023-03-04 22:24:30 +0400

    Добавляет стилизацию блока о компании и блока товары и услуги

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initAccordeons} from './modules/accordion/spoller';
+import {toggel} from './modules/toggel/toggel';
 
 // ---------------------------------
 
@@ -16,6 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   initAccordeons();
+  toggel();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
