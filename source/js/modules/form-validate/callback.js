d5b1df3132dcb3747cbef2cc99b6d02d8632bc3f
Надежда Худякова <105661918+nadegda150791@users.noreply.github.com>
2023-03-13 13:22:08 +0400

    Добавляет исправление ошибок

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,6 +1,23 @@
 
 const baseSuccessCallback = (event) => {
   // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
+  // const getData = async (successCallback, errorCallback) => {
+  //   try {
+  //     const response = await fetch(
+  //       'https://echo.htmlacademy.ru'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Ошибка загрузки данных');
+  //     }
+
+  //     const offers = await response.json();
+  //     successCallback(offers);
+  //   } catch (error) {
+  //     errorCallback(error.message);
+  //   }
+  // };
+  // getData();
 };
 
 const baseErrorCallback = (event) => {
